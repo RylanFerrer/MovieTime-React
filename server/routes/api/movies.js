b49cea4f6ac731be1da416e9e549812da7e4847b
RylanFerrer <rylanferrer@gmail.com>
2019-12-27 15:52:38 -0800

    setup front and backend

---
+++ server/routes/api/movies.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const API_KEY = require('../../key/key')
+const router = express.Router()
+
+router.get("/", (req,res) => {
+    
+})
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios')
+const app = express();
+
+const PORT  = process.env.PORT || 5000;
+
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("Hello")
+})
+app.use("/api/movies", require("./routes/api/movies"))
+
+app.listen(PORT, (req,res) => {
+    console.log(`Server is running on port ${PORT}`)
+})
