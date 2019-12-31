38bf2bb5fe7ab8765a259e4b2b203ec872713b75
RylanFerrer <rylanferrer@gmail.com>
2019-12-30 23:49:22 -0800

    added hover effects on media posters

--- client/src/components/Sliders/HomeSliderContent.js
+++ client/src/components/Sliders/HomeSliderContent.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Tilt from 'react-tilt'
 import {posterPath} from '../Helpers/helpervars'
 import {Link} from "react-router-dom"
 const HomeSliderContent = (props) => {
     const {media, mediaType} = props
     return (
         <div>
+            <Tilt className="Tilt" options={{ max : 25 }}  >
             <Link  className = "link" to = {`/${mediaType}/${media.id}`}>
                 <img alt = {media.original_title} src = {`${posterPath}${media.poster_path}`}/>
             </Link>  
+            </Tilt>
+            <h5 className = "poster-title">{mediaType === "movie" ? media.original_title : media.original_name}</h5>
         </div>
     );
 }
