94b336905ec9d32db01439551f9d5b4c982012d6
RylanFerrer <rylanferrer@gmail.com>
2020-01-05 15:29:14 -0800

    tweaked slider settings

--- client/src/components/Helpers/helpervars.js
+++ client/src/components/Helpers/helpervars.js
@@ -11,49 +11,43 @@ responsive: [
     {
     breakpoint: 2560,
     settings: {
-        slidesToShow:6,
+        slidesToShow:5,
         slidesToScroll:3
     }
     },
     {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+  {
     breakpoint: 1200,
-    settings: {
-      slidesToShow: 5,
-      slidesToScroll: 1
-    }
-  },
-  {
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 4,
-      slidesToScroll: 4
-    }
-  },
-  {
-    breakpoint: 992,
-    settings: {
-      slidesToShow: 4,
-      slidesToScroll: 4
-    }
-  },
-  {
-
-    breakpoint: 768,
     settings: {
       slidesToShow: 3,
       slidesToScroll: 3
     }
   },
   {
-  breakpoint: 767,
-  settings: {
+    breakpoint: 992,
+    settings: {
       slidesToShow: 2,
-      slidesToScroll: 1
-  }
+      slidesToScroll: 2
+    }
   },
   {
-    breakpoint: 499,
+
+    breakpoint: 768,
     settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 767,
+    settings: {
+      centerMode: true,
       slidesToShow: 1,
       slidesToScroll: 1
     }
